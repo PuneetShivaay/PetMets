b4c9af175828d4a9b43ad4a503143244b6a194c8
Puneet Kumar <puneetshivaay@gmail.com>
2025-06-07 07:36:29 +0000

    i have deleted now remove unused imports

--- src/app/dashboard/layout.tsx
+++
@@ -1,6 +0,0 @@
-import ProtectedPage from '@/components/auth/ProtectedPage';
-import type { ReactNode } from 'react';
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <ProtectedPage>{children}</ProtectedPage>;
-}
