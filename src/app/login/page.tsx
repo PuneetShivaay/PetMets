aa27a869081cde67fd5d4d7797bfb7f15817f882
Puneet Kumar <puneetshivaay@gmail.com>
2025-06-07 10:46:02 +0000

    deleted them check for any other need to delete

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/auth/LoginForm";
-
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-[calc(100vh-8rem)] items-center justify-center bg-gradient-to-br from-background to-secondary/30 p-4">
-      <LoginForm />
-    </div>
-  );
-}
